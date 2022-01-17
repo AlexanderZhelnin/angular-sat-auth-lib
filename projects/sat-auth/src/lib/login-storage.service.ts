2ef09af62e3db24064f3647bd0ac1f09c68f9232
AlexanderZhelnin <kulibin_s@bk.ru>
2022-01-17 13:44:59 +0300

    рефакторинг

--- projects/sat-auth/src/lib/login-storage.service.ts
+++ projects/sat-auth/src/lib/login-storage.service.ts
@@ -5,11 +5,20 @@ import { Injectable } from '@angular/core';
 export class LoginStorageService
 {
   /** Запомнить меня, влияет на хранение данных в локальном хранилище или сессионном */
-  public get rememberMe(): boolean { return localStorage.getItem('rememberMe') !== '0'; }
-  public set rememberMe(value: boolean) { localStorage.setItem('rememberMe', value ? '1' : '0'); }
+  public get rememberMe(): boolean
+  {
+    return localStorage.getItem('rememberMe') !== '0';
+  }
+  public set rememberMe(value: boolean)
+  {
+    localStorage.setItem('rememberMe', value ? '1' : '0');
+  }
 
   /** Получить хранилище */
-  private get storage(): Storage { return this.rememberMe ? localStorage : sessionStorage; }
+  private get storage(): Storage
+  {
+    return this.rememberMe ? localStorage : sessionStorage;
+  }
 
   /** Токен обновления */
   public get refreshToken(): string { return this.storage.getItem('refreshToken') ?? ''; }
